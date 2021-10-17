692fd67bd14ca6ae0b16700549c439251e218389
tyee2 <tyee2@wisc.edu>
2021-10-17 17:00:03 -0500

    finish UART_wrapper.sv

--- project files/UART_wrapper.sv
+++ project files/UART_wrapper.sv
@@ -5,7 +5,7 @@ module UART_wrapper(
 	input 				trmt,
 	input [7:0] 		resp,
 	output 				TX,
-	output 				cmd_rdy,
+	output reg 			cmd_rdy,
 	output [15:0] 		cmd,
 	output 				tx_done
 );
@@ -15,7 +15,7 @@ module UART_wrapper(
 
 	// SM inputs and outputs
 	logic clr_rdy, rx_rdy;
-	logic high_byte_rdy;
+	logic sel_high, set_cmd_rdy;
 	
 	logic [7:0] rx_data, high_byte;
 
@@ -43,14 +43,45 @@ module UART_wrapper(
 	
 	// state transition and output logic
 	always_comb begin
+		// default outputs
+		sel_high = 0;
+		set_cmd_rdy = 0;
+		clr_rdy = 0;
+		nxt_state = state;
 		
+		// high byte is sent first, followed by the low byte
+		case(state)
+			HIGH: begin
+				if(rx_rdy) begin
+					sel_high = 1;
+					clr_rdy = 1;
+					nxt_state = LOW;
+				end
+			end
+			
+			LOW: begin
+				if(rx_rdy) begin
+					clr_rdy = 1;
+					set_cmd_rdy = 1;
+					nxt_state = HIGH;
+				end
+			end
 	end
 	
 	// flop to save the high byte
 	always_ff @(posedge clk, negedge rst_n)
 		if(!rst_n)
 			high_byte <= 8'h00; 
-		else if(high_byte_rdy)
+		else if(sel_high)
 			high_byte <= rx_data; 
 
+	// SR flop for cmd_rdy
+	always_ff @(posedge clk, negedge rst_n)
+		if(!rst_n)
+			cmd_rdy <= 0; 
+		else if(clr_cmd_rdy)
+			cmd_rdy <= 0;
+		else if(set_cmd_rdy)
+			cmd_rdy <= 1;
+		// else hold cmd_rdy
 endmodule	
