c1f7d99c7ff06c309a61394ff916289369e450a0
tyee2 <tyee2@wisc.edu>
2021-10-24 14:42:32 -0500

    bug fixes

--- project files/CommTB.sv
+++ project files/CommTB.sv
@@ -37,6 +37,8 @@ module CommTB();
 		
 		clk = 1'b0; 
 		rst_n = 1'b0; 	
+        clr_cmd_rdy = 1'b0;
+        trmt = 0;
 		
 		@(negedge clk);
 		rst_n = 1'b1; 
@@ -46,17 +48,17 @@ module CommTB();
 		@(negedge clk);
 		snd_cmd = 1'b0;
 
-		// Testing sending/receiving cmd
+        $display("TEST 1: sending and receiving functionality for cmd=%h",cmd_in);
 		fork
 			begin: timeout_tx1
 				repeat(100000) @(posedge clk);
-				$display("ERROR: transmission never completed after 100000 cycles. check your SM for UART_tx.");
+				$display("ERROR: transmission never completed after 100000 cycles.");
 				$stop;
 			end
 			
 			begin: timeout_rx1
 				repeat(100000) @(posedge clk);
-				$display("ERROR: receiver never asserted rdy after 100000 cycles. check your SM for UART_rx.");
+				$display("ERROR: receiver never asserted rdy after 100000 cycles.");
 				$stop;
 			end
 			
@@ -64,7 +66,8 @@ module CommTB();
 				@(posedge cmd_rdy) disable timeout_rx1;
 				@(posedge cmd_snt) disable timeout_tx1;
 				
-				assert(cmd_in == cmd_out) $display("PASS: cmd received is the same as the cmd sent. cmd: %h", cmd_in);
+				assert(cmd_in === cmd_out) 
+                    $display("PASS: cmd received is the same as the cmd sent. cmd: %h", cmd_in);
 				else begin
 					$display("ERROR: Data was not sent and received properly. " +
                     "cmd received was %h and cmd sent was %h", cmd_out, cmd_in); 
@@ -72,6 +75,100 @@ module CommTB();
 				end
 			end
 		join
+
+        @(negedge clk);
+        snd_cmd = 1'b1; 
+		cmd_in = 16'h1234;
+
+		@(negedge clk);
+		snd_cmd = 1'b0;
+
+        $display("TEST 2: deasserting cmd_rdy with another command." +
+        "testing sending/receiving functionality for cmd=%h.",cmd_in);
+		fork
+			begin: timeout_tx2
+				repeat(100000) @(posedge clk);
+				$display("ERROR: transmission never completed after 100000 cycles.");
+				$stop;
+			end
+			
+			begin: timeout_rx2
+				repeat(100000) @(posedge clk);
+				$display("ERROR: receiver never asserted rdy after 100000 cycles.");
+				$stop;
+			end
+			
+			begin
+				@(posedge cmd_rdy) disable timeout_rx2;
+				@(posedge cmd_snt) disable timeout_tx2;
+				
+				assert(cmd_in === cmd_out) 
+                    $display("PASS: cmd received is the same as the cmd sent. cmd: %h", cmd_in);
+				else begin
+					$display("ERROR: Data was not sent and received properly. " +
+                    "cmd received was %h and cmd sent was %h", cmd_out, cmd_in); 
+					$stop;
+				end
+
+                assert(cmd_rdy === 0) 
+                    $display("PASS: cmd_rdy was deasserted properly.");
+				else begin
+					$display("ERROR: cmd_rdy was not deasserted with another cmd."); 
+					$stop;
+				end
+			end
+		join
+
+        $display("TEST 3: asserting clr_cmd_rdy");
+        @(negedge clk);
+        snd_cmd = 1'b1; 
+		cmd_in = 16'hFFFF;
+
+		@(negedge clk);
+		snd_cmd = 1'b0;
+        fork
+			begin: timeout_tx3
+				repeat(100000) @(posedge clk);
+				$display("ERROR: transmission never completed after 100000 cycles.");
+				$stop;
+			end
+			
+			begin: timeout_rx3
+				repeat(100000) @(posedge clk);
+				$display("ERROR: receiver never asserted rdy after 100000 cycles.");
+				$stop;
+			end
+			
+			begin
+				@(posedge cmd_rdy) disable timeout_rx3;
+				@(posedge cmd_snt) disable timeout_tx3;
+				
+				assert(cmd_in === cmd_out) 
+                    $display("PASS: cmd received is the same as the cmd sent. cmd: %h", cmd_in);
+				else begin
+					$display("ERROR: Data was not sent and received properly. " +
+                    "cmd received was %h and cmd sent was %h", cmd_out, cmd_in); 
+					$stop;
+				end
+			end
+		join
+
+        repeat(100) @(negedge clk);
+        clr_cmd_rdy = 1'b1;
+        @(negedge clk);
+        clr_cmd_rdy = 1'b0;
+        @(posedge clk);
+        @(negedge clk);
+
+        assert(cmd_rdy === 0)
+            $display("PASS: cmd_rdy knocked down by clr_cmd_rdy");
+        else begin
+            $display("ERROR: cmd_rdy was not deasserted by clr_cmd_rdy");
+            $stop;
+        end
+
+        $display("All tests passed, good job.");
+        $stop;
     end
 
     always #5 clk = ~clk;
