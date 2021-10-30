bd5c7e3f8712b2762a55d355d2b64122316c0c06
tyee2 <tyee2@wisc.edu>
2021-10-30 00:59:15 -0500

    update SPI tb

--- project files/SPI_mnrch_tb.sv
+++ project files/SPI_mnrch_tb.sv
@@ -1,11 +1,12 @@
 module SPI_mnrch_tb();
 
-    reg clk,rst_n,wrt;
-    reg [15:0] wt_data;
-    wire [15:0] rd_data;
-    wire SS_n,SCLK,MOSI,MISO,done;
-    wire INT;
+    logic clk, rst_n;
+    logic wrt;
+    logic [15:0] wt_data, rd_data;
+    logic SS_n, SCLK, MOSI, MISO, done;
+    logic INT;
 
+    // instantiate SPI
     SPI_mnrch iM(
         .clk(clk),
         .rst_n(rst_n),
@@ -28,18 +29,20 @@ module SPI_mnrch_tb();
     );
 
     initial begin
-        clk=0;
-        rst_n=0;
-        wrt=1;
-        wt_data=16'h8Fxx;
+        clk = 0;
+        rst_n = 0;
+        wrt = 1;
+        wt_data = 16'h8Fxx;
 
         @(posedge clk);
-        @(negedge clk) rst_n=1;
-        @(posedge clk) wrt=0;
+        @(negedge clk); 
+        rst_n = 1;
+        @(posedge clk); 
+        wrt=0;
 
         @(posedge done) 
         if(rd_data[7:0] !== 8'h6A) begin
-            $display("The WHO_AM_I register was not read correctly");
+            $display("WHO_AM_I register did not return the correct value. Expected 6A, got %h", rd_data[7:0]);
             $stop();
         end
 
